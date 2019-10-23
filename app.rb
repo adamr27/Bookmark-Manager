887410b4ce830c2627ba3c9abc7d7c54583d762f
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-10-23 15:21:56 +0100

    step 10 complete

--- app.rb
+++ app.rb
@@ -19,10 +19,15 @@ class BookmarkManager < Sinatra::Base
     erb(:"bookmarks/new")
   end
 
+  # post '/bookmarks' do
+  #   url = params['url']
+  #   connection = PG.connect(dbname: 'bookmark_manager_test')
+  #   connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+  #   redirect '/bookmarks'
+  # end
+
   post '/bookmarks' do
-    url = params['url']
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    Bookmark.create(url: params[:url])
     redirect '/bookmarks'
   end
 
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -11,10 +11,18 @@ describe Bookmark do
       connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
 
       bookmarks = Bookmark.all
-      
+
       expect(bookmarks).to include('http://www.makersacademy.com')
       expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include('http://www.google.com')
+
+    end
+  end
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
     end
   end
 
